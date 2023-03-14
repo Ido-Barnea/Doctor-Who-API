564f229cd0aca5cce04e631788646d3feed6cb4d
Ido-Barnea <dodo.barnea@gmail.com>
2023-03-14 20:34:01 +0200

    Decresed "id" by one because IDs in JSON start from one.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ module.exports = app;
 const api = require('./routes/api')
 
 app.use('/api', api);
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.status(200).json('Welcome to Doctor Who API!');
+    res.sendFile('./public/index.html');
+    //res.status(200).json('Welcome to Doctor Who API!');
 });
