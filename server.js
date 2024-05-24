b5a2a8b2484bfcfb84e93f16423e6f4fdadd6298
Ido-Barnea <dodo.barnea@gmail.com>
2024-05-24 18:42:55 +0300

    Refactored server.js

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const http = require('http');
-const app = require('./app')
+const express = require('express');
+const path = require('path');
 
-const port = process.env.port || 3000;
-const server = http.createServer(app);
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const api = require('./routes/api')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', api);
+
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome to Doctor Who API! Visit /documentation for the documentation and /api for the API.');
+});
+
+app.get('/documentation', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'public/documentation/documentation.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`[Server] Listening on port ${PORT}.`);
 });
